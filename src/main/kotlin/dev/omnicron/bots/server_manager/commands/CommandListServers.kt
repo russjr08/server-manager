8ebbfd89013db63ccfffeb66f9b564e9fc6bbae4
Russell Richardson <Russell@KronosAD.com>
2021-08-25 13:55:31 -0400

    The list server command now includes server's power state in embed

--- src/main/kotlin/dev/omnicron/bots/server_manager/commands/CommandListServers.kt
+++ src/main/kotlin/dev/omnicron/bots/server_manager/commands/CommandListServers.kt
@@ -1,5 +1,6 @@
 package dev.omnicron.bots.server_manager.commands
 
+import com.mattmalec.pterodactyl4j.UtilizationState
 import com.mattmalec.pterodactyl4j.client.entities.PteroClient
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Message
@@ -16,8 +17,22 @@ class CommandListServers(private val pteroApi: PteroClient): ICommand {
             embedBuilder.setTitle("List of Registered Servers")
             embedBuilder.setDescription("Each item is the server's name, and the node it's present on")
 
+            val emoteStarted = "☑"
+            val emoteInProgress = "🔄"
+            val emoteStopped = "❌"
+            var currentEmote = emoteInProgress
+
             servers.forEach { server ->
-                embedBuilder.addField(server.name, server.node, false)
+                val utilization = server.retrieveUtilization().execute()
+                currentEmote = when(utilization.state) {
+                    UtilizationState.STARTING -> emoteInProgress
+                    UtilizationState.STOPPING -> emoteInProgress
+                    UtilizationState.RUNNING -> emoteStarted
+                    UtilizationState.OFFLINE -> emoteStopped
+                    else -> emoteInProgress
+                }
+                embedBuilder.addField("$currentEmote ${server.name}", server.node, false)
+
             }
 
             embedBuilder.setFooter("ServerManager - Russell Richardson")
